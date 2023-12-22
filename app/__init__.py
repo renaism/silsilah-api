007d66308320bf4eb2f70f2cd6dd0453308db54f
Rezza Nafi <zafitract@gmail.com>
2023-12-22 17:12:21 +0700

    feat: add cors middleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from app.config import Config
 from app.routes.v1.family import router as family_router
 from app.routes.v1.person import router as person_router
 from app.routes.v1.tree import router as tree_router
@@ -10,13 +12,21 @@ app = FastAPI(
     description="REST API for Silsilah App"
 )
 
-
-@app.get("/")
-async def root() -> dict:
-    return { "msg": "OK!" }
-
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=Config.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Routers
 app.include_router(person_router)
 app.include_router(family_router)
 app.include_router(tree_router)
+
+
+@app.get("/")
+async def root() -> dict:
+    return { "msg": "OK!" }
--- app/config.py
+++ app/config.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+class Config:
+    CORS_ORIGINS = os.getenv("CORS_ORIGINS", default="").split(",")
+
+
 class DBConfig:
     HOST = os.getenv("DB_HOST", default="127.0.0.1:5432")
     NAME = os.getenv("DB_NAME", 'silsilah')
